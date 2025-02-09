e7a1d3bb1743cc2af222ddb580771d4f39eb4dfd
Parth Rethaliya <parthrethaliya6105@gmail.com>
2025-02-09 22:46:14 +0530

    Add project files.

---
+++ Feb-7 List Task/BAL/Student.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Feb_7_List_Task.Model;
+
+
+namespace Feb_7_List_Task.BAL
+{
+
+    public class Student
+    {
+        public List<StudentModel3> GetStudentList()
+        {
+            List<StudentModel3> lstStudent = new();
+            lstStudent.Add(new StudentModel3()
+            {
+                Id = 1,
+                Name = "Jaypal",
+                DOB = DateTime.Now.AddYears(-25),
+                Gender = "M",
+                RollNo = "RL_1",
+                Std = 12,
+                SurName = "Vasveliya"
+            });
+            lstStudent.Add(new StudentModel3()
+            {
+                Id = 2,
+                Name = "Milan",
+                DOB = DateTime.Now.AddYears(-22),
+                Gender = "M",
+                RollNo = "RL_2",
+                Std = 12,
+                SurName = "Gandhi"
+            });
+            lstStudent.Add(new StudentModel3()
+            {
+                Id = 3,
+                Name = "Khushi",
+                DOB = DateTime.Now.AddYears(-24),
+                Gender = "F",
+                RollNo = "RL_3",
+                Std = 12,
+                SurName = "Mehta"
+            });
+
+            return lstStudent;
+        }
+
+        public List<string> GetStudentBasedOnAge(int age) //5
+        {
+            List<StudentModel3> lstStudent = GetStudentList(); //3
+            int year = DateTime.Now.AddYears(-age).Year; //2020
+            List<StudentModel3> flterList = new(); //0
+            foreach (StudentModel3 student in lstStudent)
+            {
+                if (year > student.DOB.Year)//2020 < 2000 
+                {
+                    flterList.Add(student);
+                }
+            }
+            return flterList.Select(x => x.Name).ToList();
+        }
+        public List<string> GetFullName() //show name + surname
+        {
+            List<StudentModel3> lstStudent = GetStudentList();
+            List<string> fullnames = new();
+
+            foreach (StudentModel3 student in lstStudent)
+            {
+                string fullname = student.Name + " " + student.SurName; fullnames.Add(fullname);
+            }
+            return fullnames;
+
+        }
+        public List<string> AllNameCaps() //all name Capital
+        {
+            List<StudentModel3> studentp = GetStudentList();
+
+
+            foreach (StudentModel3 student in studentp)
+            {
+
+
+                Console.WriteLine($"{student.Name?.ToUpper()}");
+
+            }
+            return studentp?.Select(x => x.Name).ToList();
+        }
+        public List<StudentModel3> AddNewStudent(int id, string Name, string Surname, string Rollno, string Gender, DateTime Dob, int std) //add new student 
+        {
+            List<StudentModel3> lstStudent = GetStudentList();
+            StudentModel3 student1 = new StudentModel3 { Id = id, Name = Name, SurName = Surname, RollNo = Rollno, Gender = Gender, DOB = Dob, Std = std };
+            lstStudent.Add(student1);
+            foreach (StudentModel3 student in lstStudent)
+            {
+                Console.WriteLine("ID:{0},StudentName is:{1} ,Surname is:{2}, Roll-no:{3} , Gender:{4} , Dob:{5} , Std:{6} ",student.Id,student.Name,student.SurName,student.RollNo,student.Gender,student.DOB,student.Std);
+
+            }
+
+            return lstStudent;
+
+
+        }
+
+        //public List<string> StartWith(string? charactor)
+        //{
+        //    List<StudentModel3> lstStudent = GetStudentList();
+        //    return lstStudent.Where(Student => Student.Name.StartsWith(charactor.ToString()
+
+        //}
+
+
+
+
+
+
+            //public List<string> GetStudentAgeRange(int startyear, int endyear) //Get Student age range
+
+            // {
+            //List<StudentModel3> lstStudent = GetStudentList();
+            //List<StudentModel3> filterList4 = new(); //0
+
+
+            //foreach (StudentModel3 student2 in lstStudent)
+            //{
+            //    string student3 = student2.Where(student2.DOB.Year >= startyear && student2.DOB.Year <= endyear).Add(student2); 
+             
+            //        Console.WriteLine(student3);
+         
+
+            //}
+
+                   
+
+            //return filterList4;
+
+        //}
+
+
+        public List <string> ShowDOB() //show Dob
+         {
+            List<StudentModel3> lstStudent = GetStudentList();
+            List<string> list3 = new List<string>();
+            foreach (StudentModel3 student in lstStudent)
+            {  
+              string birthdate =student.DOB.ToString("dd MMMM yyyy");
+                list3.Add(birthdate);
+
+               
+
+            }
+
+            return list3;
+
+          }
+        //public List<string> ChangeSurname(int id,string surname) 
+        //{
+        //    List<StudentModel3> list2 = GetStudentList();
+        //    foreach (StudentModel3 student in list2)
+        //    {
+        //        if (id == student.Id)
+        //        {
+        //            string student3 = student.SurName.Replace;
+        //        }
+              
+        //    }
+
+
+        //}
+
+        public List<string> GetUpdatedStudentlist()//Repalce method 
+        {
+            List<StudentModel3> list2 = GetStudentList();
+            List<string> strings = new List<string>();
+            foreach (StudentModel3 student in list2)
+            {
+                 string rollno = student.RollNo.Replace("RL", "#");
+               
+                strings.Add(rollno);    
+            }
+
+            return  strings;
+        }
+
+
+    }
+
+
+
+
+} 
+
+
+
+
+
---
+++ Feb-7 List Task/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Feb_7_List_Task.BAL;
+using Feb_7_List_Task.Model;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Student objStudent = new();
+//List<string> lstStudentName = objStudent.GetStudentBasedOnAge(24);
+//foreach (string studentName in lstStudentName)
+//{
+//    Console.WriteLine(studentName);
+//}
+//List<StudentModel3> lstStudent = objStudent.GetStudentList();
+
+//for (int i = 0; i < lstStudent.Count; i++)
+//{
+//   Console.WriteLine(lstStudent[i].Name);
+//}
+
+//foreach (StudentModel3 student in lstStudent)
+//{
+//    if (student.Gender == "F")
+//    {
+//        Console.WriteLine(student.Name);
+//    }
+//}
+//if(student.DOB)
+
+//Console.WriteLine(student.Gender == "F" ? student.Name : "Condition Not Satisfied");
+
+//List<string> lstStudentName2 = objStudent.GetStudentAgeRange(11,25);
+//foreach (string studentName in lstStudentName2)
+//{
+//    Console.WriteLine(studentName);
+//}
+
+//List<string> lstStudent2 = objStudent.GetFullName();
+//foreach (string name in lstStudent2)
+//{
+//    Console.WriteLine(name);
+//}
+
+//List<string> studentR = objStudent.AllNameCaps();
+
+
+
+//List <StudentModel3> newstudent1 = objStudent.AddNewStudent(4, "Parth", "Rethaliya", "RL_4", "M", DateTime.Now.AddYears(-26), 12);
+
+//List<string> lstStudentName2 = objStudent.StartWith("p");
+//foreach (string studentName in lstStudentName2)
+//{
+//    Console.WriteLine(studentName);
+//}
+//List<string> lstStudentName = objStudent.ShowDOB();
+
+//foreach (string s in lstStudentName)
+//{
+//    Console.WriteLine(s);
+//}
+List<string> students2 = objStudent.GetUpdatedStudentlist();
+
+foreach (string student in students2)
+{
+    Console.WriteLine(student);
+}
